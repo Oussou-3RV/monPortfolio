9efcd7aaac19f097cde60eee165203c2eb923d93
OUSMANE BALDE <baldemarc224@gmail.com>
2025-08-05 16:38:28 -0400

    j'ai implementé le header

---
+++ src/components/heroSection.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Hello this is a heroSction</h2>
+</template>
---
+++ src/components/navBar.vue
@@ -0,0 +1,52 @@
+<template>
+    <header class="flex justify-between items-center p-6 bg-opacity-50 z-20">
+        <div class="text-white text-3xl font-bold">LOGO</div>
+
+        <!-- Mobile Toggle Button -->
+        <div class="md:hidden z-30">
+            <button type="button" class="block focus:outline-none" @click="isMenuOpen = !isMenuOpen">
+            <span v-if="isMenuOpen" class="text-5xl close transition hover:color-primary ease-linear">
+                <i class="fa-solid fa-xmark" style="color: #fafafa; font-size: 3rem;"></i>
+            </span>
+            <span v-else class="text-5xl menu">
+                <i class="fa-solid fa-bars" style="color: #fafafa; font-size: 3rem;"></i>
+            </span>
+        </button>
+        </div>
+
+        <!-- Nav-bar link for desktop-->
+         <nav :class="['fixed inset-0 z-20 flex flex-col items-center justify-center bg-[#111827] md:bg-transparent md:flex md:justify-between md:flex-row',
+          isMenuOpen? 'block':'hidden' ]">
+            <ul class="flex flex-col items-center space-y-5 md:flex-row md:items-start md:space-x-5 md:space-y-0">
+                <li v-for="item in Menu" :key="item.name" @click="scrollToSection(item.href)">
+                    <a :href="item.href" class="block text-white transition hover:text-primary ease-linear text-2xl md:text-lg">
+                        {{ item.name }}
+                    </a>
+                </li>
+            </ul>
+         </nav>
+    </header>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const Menu = ref([
+    {name:'services',href:'#services'},
+    {name:'About Me',href:'#about'},
+    {name:'Skills',href:'#skills'},
+    {name:'Projects',href:'#projects'},
+    {name:'Testimonials',href:'#testimonials'},
+    {name:'Contact',href:'#contact'},
+])
+
+const isMenuOpen = ref(false)
+
+const scrollToSection = (href)=>{
+    isMenuOpen.value = false
+    const section = document.querySelector(href)
+    if(section){
+        section.scrollIntoView({bahavior : 'smooth'});
+    }
+}
+</script>
