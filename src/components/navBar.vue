fdd8e650aa71c296788c61033e3e027f8aec8b1c
OUSMANE BALDE <baldemarc224@gmail.com>
2025-08-06 17:12:13 -0400

    ajout d'image

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -5,26 +5,27 @@
         <!-- Mobile Toggle Button -->
         <div class="md:hidden z-30">
             <button type="button" class="block focus:outline-none" @click="isMenuOpen = !isMenuOpen">
-            <span v-if="isMenuOpen" class="text-5xl close transition hover:color-primary ease-linear">
-                <i class="fa-solid fa-xmark" style="color: #fafafa; font-size: 3rem;"></i>
-            </span>
-            <span v-else class="text-5xl menu">
-                <i class="fa-solid fa-bars" style="color: #fafafa; font-size: 3rem;"></i>
-            </span>
-        </button>
+                <span v-if="isMenuOpen" class="text-5xl close">
+                    <i class="fa-solid fa-xmark" style="color: #fafafa; font-size: 2.3rem;"></i>
+                </span>
+                <span v-else class="text-5xl menu">
+                    <i class="fa-solid fa-bars" style="color: #fafafa; font-size: 2.3rem;"></i>
+                </span>
+            </button>
         </div>
 
-        <!-- Nav-bar link for desktop-->
-         <nav :class="['fixed inset-0 z-20 flex flex-col items-center justify-center bg-[#111827] md:relative md:bg-transparent md:flex  md:flex-row md:justify-between',
-          isMenuOpen? 'block':'hidden' ]">
+        <!-- Nav-bar link -->
+        <nav :class="['fixed inset-0 z-20 flex flex-col items-center justify-center bg-[#111827] md:relative md:bg-transparent md:flex  md:flex-row md:justify-between',
+            isMenuOpen ? 'block' : 'hidden']">
             <ul class="flex flex-col items-center space-y-5 md:flex-row md:space-x-5 md:space-y-0">
                 <li v-for="item in Menu" :key="item.name" @click="scrollToSection(item.href)">
-                    <a :href="item.href" class="block text-white transition hover:text-primary ease-linear text-2xl md:text-lg">
+                    <a :href="item.href"
+                        class="block text-white transition hover:text-primary ease-linear text-2xl md:text-lg">
                         {{ item.name }}
                     </a>
                 </li>
             </ul>
-         </nav>
+        </nav>
     </header>
 </template>
 
@@ -32,21 +33,21 @@
 import { ref } from 'vue';
 
 const Menu = ref([
-    {name:'services',href:'#services'},
-    {name:'About Me',href:'#about'},
-    {name:'Skills',href:'#skills'},
-    {name:'Projects',href:'#projects'},
-    {name:'Testimonials',href:'#testimonials'},
-    {name:'Contact',href:'#contact'},
+    { name: 'services', href: '#services' },
+    { name: 'About Me', href: '#about' },
+    { name: 'Skills', href: '#skills' },
+    { name: 'Projects', href: '#projects' },
+    { name: 'Testimonials', href: '#testimonials' },
+    { name: 'Contact', href: '#contact' },
 ])
 
 const isMenuOpen = ref(false)
 
-const scrollToSection = (href)=>{
+const scrollToSection = (href) => {
     isMenuOpen.value = false
     const section = document.querySelector(href)
-    if(section){
-        section.scrollIntoView({bahavior : 'smooth'});
+    if (section) {
+        section.scrollIntoView({ bahavior: 'smooth' });
     }
 }
 </script>
